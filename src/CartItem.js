9b5f52485b32468b4821f012cf4cb6b73a3e636a
Murtaza Pakawala <mypaka52@gmail.com>
2022-05-18 18:11:50 +1000

    adding style to the CartItem component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "mobile phone",
+      price: 99,
+      qty: 4,
+      img: "",
+    };
+  }
   render() {
+    const { title, price, qty } = this.state;
     return (
       <div className="cart-item">
         <div className="left-block">
@@ -15,10 +25,25 @@ class CartItem extends React.Component {
           />
         </div>
         <div className="right-block">
-          <div>phone</div>
-          <div>Rs 999</div>
-          <div>qnt:5</div>
+          <div>{title}</div>
+          <div>price : {price}</div>
+          <div>quantity : {qty}</div>
           <div className="cart-item-actoins">
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://as1.ftcdn.net/v2/jpg/03/73/49/86/1000_F_373498649_nBxauQ0ipBSVrVcMpWWVmTpXu3BLvRyY.jpg"
+            ></img>
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://as2.ftcdn.net/v2/jpg/01/07/62/07/1000_F_107620769_UwNVSoXnKS4VNcOKoZjPohlEPn83oE38.jpg"
+            ></img>
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://as2.ftcdn.net/v2/jpg/01/90/89/15/1000_F_190891550_N7uKp2aHE3mOc20dmtDytj7atgvbhdOu.jpg"
+            ></img>
             {/* buttons for the actions */}
           </div>
         </div>
